4fcd44c4d462812cf5d71b2fd4104309cc7eafc3
thfuchs <thomasfuchs-re@web.de>
2020-08-27 16:30:28 +0000

    update dow30 (ordering) and initial commit apple_fcf

--- R/data.R
+++ R/data.R
@@ -4,15 +4,25 @@
 #' @usage data(dow30)
 #' @keywords datasets
 #'
-#' @format A data.table with 3600 rows and 7 variables:
+#' @format A data.table with 3600 rows and 8 variables:
 #' \describe{
 #'   \item{company}{Dow Jones listed companies in (August 2020)}
+#'   \item{ticker}{bbreviation of company title}
 #'   \item{date}{Publishing date of (quarterly) report in POSIX format.
 #'     Month and year of interest}
-#'   \item{cap}{Bloomberg: CF_CAP_EXPEND_PRPTY_ADD}
-#'   \item{cff}{Financing Cash Flow - Bloomberg: CF_CASH_FROM_FNC_ACT}
-#'   \item{cfi}{Investing Cash Flow - Bloomberg: CF_CASH_FROM_INV_ACT}
-#'   \item{cfo}{Operating Cash Flow - Bloomberg: CF_CASH_FROM_OPER}
 #'   \item{fcf}{Free Cash Flow - Bloomberg: CF_FREE_CASH_FLOW}
+#'   \item{cfo}{Operating Cash Flow - Bloomberg: CF_CASH_FROM_OPER}
+#'   \item{cfi}{Investing Cash Flow - Bloomberg: CF_CASH_FROM_INV_ACT}
+#'   \item{cff}{Financing Cash Flow - Bloomberg: CF_CASH_FROM_FNC_ACT}
+#'   \item{cap}{Bloomberg: CF_CAP_EXPEND_PRPTY_ADD}
 #' }
 "dow30"
+
+#' Quarterly reported free cash flow from Apple as ts object from 1995 to 2020.
+#'
+#' @docType data
+#' @usage data(apple_fcf)
+#' @keywords datasets
+#'
+#' @format A quarterly ts object with from 1995 to 2020
+"apple_fcf"
