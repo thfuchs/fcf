3a8eea76b71097c26fb303c22d31b114713ccd3e
thfuchs <thomasfuchs-re@web.de>
2020-09-02 14:38:33 +0000

    Initial commit arima comparison model

---
+++ inst/arima.R
@@ -0,0 +1,12 @@
+apple <- fcf::apple_fcf
+train <- subset(apple, end = length(apple) - 8)
+
+library(forecast)
+library(ggplot2)
+
+# 1. auto.arima
+arima_fcf <- auto.arima(train, stepwise = FALSE, parallel = TRUE, num.cores = NULL)
+autoplot(forecast(arima_fcf))
+summary(arima_fcf)
+checkresiduals(arima_fcf)
+accuracy(forecast(arima_fcf), apple)
