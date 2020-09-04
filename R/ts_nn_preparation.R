eeb8471965197e1e179335f6252651689a8c2668
thfuchs <thomasfuchs-re@web.de>
2020-09-04 17:53:56 +0000

    multiple lags allowed in ts_nn_preparation

--- R/ts_nn_preparation.R
+++ R/ts_nn_preparation.R
@@ -1,9 +1,9 @@
 #' Timeseries data preparation for neural network Keras models
 #'
 #' The following steps are proceeded:
-#' - New column "lag_value", lagged by `lag_setting`
+#' - New columns "value_lagX", with X for #lags, specified by `lag_setting`
 #' - Split in train, validation and test split
-#' - Transformation in arrays for Keras
+#' - Transformation to 3D arrays for Keras
 #'
 #' @param data balanced timeseries data
 #' @param lag_setting by how much is y lagged
@@ -17,39 +17,48 @@
 #'
 ts_nn_preparation <- function(
   data,
-  lag_setting = 8,
+  lag_setting = 1:4,
   length_val = 16,
   length_test = 8
 ) {
 
   n <- nrow(data)
-  n_train <- 1:(n - (length_val + length_test) - lag_setting)
-  n_val <- (n - (length_val + length_test) - lag_setting):(n - length_test - lag_setting)
+  max_lag <- max(lag_setting)
+  n_train <- 1:(n - (length_val + length_test) - max_lag)
+  n_val <- (n - (length_val + length_test) - max_lag):(n - length_test - max_lag)
   n_val <- n_val[-1]
-  n_test <- (n - length_test - lag_setting):(n - lag_setting)
+  n_test <- (n - length_test - max_lag):(n - max_lag)
   n_test <- n_test[-1]
 
   # 2D And 3D Train/Test Arrays
-  data[, value_lag := shift(value, n = lag_setting, type = "lag")]
-  data <- data[!is.na(value_lag)]
+  add_shift(data, cols = "value", nlags = lag_setting, type = "lag")
+  message(sprintf(
+    "%s lag(s) created. Now %s rows missing - removing.",
+    max_lag,
+    data[is.na(get(paste0("value_lag", max_lag))), .N]
+  ))
+  data <- data[!is.na(get(paste0("value_lag", max_lag)))]
 
   train_DT <- data[n_train]
-  x_train_vec <- train_DT$value_lag
-  x_train_arr <- array(data = x_train_vec, dim = c(length(x_train_vec), 1, 1))
+  x_train_mat <- as.matrix(train_DT[, .SD, .SDcols = patterns("^value_lag")])
+  x_train_dim <- c(nrow(x_train_mat), length(lag_setting), 1)
+  x_train_arr <- array(data = x_train_mat, dim = x_train_dim)
 
   y_train_vec <- train_DT$value
   y_train_arr <- array(data = y_train_vec, dim = c(length(y_train_vec), 1))
 
   val_DT <- data[n_val]
-  x_val_vec <- val_DT$value_lag
-  x_val_arr <- array(data = x_val_vec, dim = c(length(x_val_vec), 1, 1))
+  x_val_mat <- as.matrix(val_DT[, .SD, .SDcols = patterns("^value_lag")])
+  x_val_dim <- c(nrow(x_val_mat), length(lag_setting), 1)
+  x_val_arr <- array(data = x_val_mat, dim = x_val_dim)
 
   y_val_vec <- val_DT$value
   y_val_arr <- array(data = y_val_vec, dim = c(length(y_val_vec), 1))
 
   test_DT <- data[n_test]
-  x_test_vec <- test_DT$value_lag
-  x_test_arr <- array(data = x_test_vec, dim = c(length(x_test_vec), 1, 1))
+  x_test_mat <- as.matrix(test_DT[, .SD, .SDcols = patterns("^value_lag")])
+  x_test_dim <- c(nrow(x_test_mat), length(lag_setting), 1)
+  x_test_arr <- array(data = x_test_mat, dim = x_test_dim)
 
   y_test_vec <- test_DT$value
   y_test_arr <- array(data = y_test_vec, dim = c(length(y_test_vec), 1))
