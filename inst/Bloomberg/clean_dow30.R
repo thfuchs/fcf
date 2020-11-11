97022bd984e51be41035e13bb0a002332d96677a
TFcfgo <thomas.fuchs@cfgo.de>
2020-11-11 20:08:51 +0100

    add section to remove outlier in clean_dow30.R

--- inst/Bloomberg/clean_dow30.R
+++ inst/Bloomberg/clean_dow30.R
@@ -1,6 +1,7 @@
 # Data investigations - Save data to dow30_clean once all missing values and
 # time series leaps are removed
 
+### Overview -------------------------------------------------------------------
 data <- fcf::dow30
 
 # summarytools::descr(
@@ -14,6 +15,9 @@ min_date <- min(dow30$date, na.rm = TRUE)
 max_date <- max(dow30$date, na.rm = TRUE)
 n <- round(as.numeric(difftime(max_date, min_date, units = "weeks")/52.25) * 4) + 1
 
+
+### Cleaning missing values ----------------------------------------------------
+
 # How many NA's do we have per column per ticker?
 data[, .(
   n_net_income = sum(complete.cases(net_income)),
@@ -23,13 +27,23 @@ data[, .(
 ), by = "ticker"]
 
 # Filter all ticker containing at least 1 NA for NI, Rev, EBIT or EBITDA
-dow30_clean <- data[
+dow30_na <- data[
   , .SD[sum(complete.cases(.SD)) == n],
   by = ticker,
   .SDcols = c("date", "net_income", "ebit", "ebitda", "revenue")
 ]
 
 # Still any missing value?
-dow30_clean[!complete.cases(ebit, ebitda, revenue, net_income), .N, by = "ticker"]
+dow30_na[!complete.cases(ebit, ebitda, revenue, net_income), .N, by = "ticker"]
 
-# save(dow30_clean, file = "data/dow30_clean.rda")
+
+### Detect and remove outlier --------------------------------------------------
+
+dow30_outlier <- tsEDA::detect_outlier(dow30_na, value_col = "ebit")
+replacement_values <- dow30_outlier[Outlier == "replacement", ebit]
+dow30_outlier[Outlier == "yes", ebit := replacement_values]
+dow30_clean <- dow30_outlier[Outlier != "replacement", .SD, .SDcols = c("ticker", "date", "ebit")]
+data.table::setnames(dow30_clean, c("date", "ebit"), c("index", "value"))
+
+### Save cleaned data ----------------------------------------------------------
+save(dow30_clean, file = "data/dow30_clean.rda")
