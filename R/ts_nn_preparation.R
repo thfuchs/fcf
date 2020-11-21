9bfaaad285a5e7c7fde8d75a0247802522ff94bc
thfuchs <thomasfuchs-re@web.de>
2020-11-21 16:39:17 +0000

    use tsteps for lag_setting

--- R/ts_nn_preparation.R
+++ R/ts_nn_preparation.R
@@ -1,44 +1,59 @@
 #' Timeseries data preparation for neural network Keras models
 #'
 #' The following steps are proceeded:
-#'   - New columns "value_lagX", with X for #lags, specified by `lag_setting`
 #'   - Split in train, validation and test split
 #'   - Transformation to 3D arrays for Keras
 #'
-#' @param data balanced timeseries data
-#' @param lag_setting by how much is y lagged
+#' @param data balanced univariate time series as data.table object. Lagged
+#'   columns in the form `value_lagX` required, where X represents number of lag
+#' @param tsteps number of lagged time steps
 #' @param length_val length of validation set
 #' @param length_test length of test set
 #'
 #' @return list of "X" and "Y" each containing "train", "val" and "test" arrays
 #' @export
-ts_nn_preparation <- function(
-  data,
-  lag_setting = 1:4,
-  length_val = 16,
-  length_test = 8
-) {
+#'
+#' @examples
+#' data <- fcf::DT_unh
+#' add_shift(data, cols = "value", nlags = 1:2, type = "lag")
+#' data <- unh[!is.na(get(paste0("value_lag", lag_upper)))]
+#'
+#' ts_nn_preparation(data, length_val = 6, length_test = n_test)
+ts_nn_preparation <- function(data, tsteps, length_val = 16L, length_test = 8L) {
 
+  ### Checks -------------------------------------------------------------------
+  testr::check_class(data, "data.table", "ts_nn_preparation")
+  testr::check_class(tsteps, "integer", "ts_nn_preparation")
+  testr::check_class(length_val, "integer", "ts_nn_preparation")
+  testr::check_class(length_test, "integer", "ts_nn_preparation")
+
+  if (sum(names(data) %like% "value_lag[0-9]") < tsteps) rlang::abort(
+    message = "`data` must be a data.table with at least `tsteps` lagged columns",
+    class = "ts_nn_preparation_data_error"
+  )
+  if (length(tsteps) != 1) rlang::abort(
+    message = "`tsteps` must be an integer of length 1.",
+    class = "ts_nn_preparation_tsteps_error"
+  )
+  if (length(length_val) != 1) rlang::abort(
+    message = "`length_val` must be an integer of length 1.",
+    class = "ts_nn_preparation_length_val_error"
+  )
+  if (length(length_test) != 1) rlang::abort(
+    message = "`length_test` must be an integer of length 1.",
+    class = "ts_nn_preparation_length_test_error"
+  )
+
+  ### Function -----------------------------------------------------------------
   n <- nrow(data)
-  max_lag <- max(lag_setting)
-  n_train <- 1:(n - (length_val + length_test) - max_lag)
-  n_val <- (n - (length_val + length_test) - max_lag):(n - length_test - max_lag)
-  n_val <- n_val[-1]
-  n_test <- (n - length_test - max_lag):(n - max_lag)
-  n_test <- n_test[-1]
+  n_train <- 1:(n - length_val - length_test)
+  n_val <- (n - length_val - length_test + 1):(n - length_test)
+  n_test <- (n - length_test + 1):n
 
   # 2D And 3D Train/Test Arrays
-  add_shift(data, cols = "value", nlags = lag_setting, type = "lag")
-  message(sprintf(
-    "%s lag(s) created. Now %s rows missing - removing.",
-    max_lag,
-    data[is.na(get(paste0("value_lag", max_lag))), .N]
-  ))
-  data <- data[!is.na(get(paste0("value_lag", max_lag)))]
-
   train_DT <- data[n_train]
   x_train_mat <- as.matrix(train_DT[, .SD, .SDcols = patterns("^value_lag")])
-  x_train_dim <- c(nrow(x_train_mat), length(lag_setting), 1)
+  x_train_dim <- c(nrow(x_train_mat), tsteps, 1)
   x_train_arr <- array(data = x_train_mat, dim = x_train_dim)
 
   y_train_vec <- train_DT$value
@@ -46,7 +61,7 @@ ts_nn_preparation <- function(
 
   val_DT <- data[n_val]
   x_val_mat <- as.matrix(val_DT[, .SD, .SDcols = patterns("^value_lag")])
-  x_val_dim <- c(nrow(x_val_mat), length(lag_setting), 1)
+  x_val_dim <- c(nrow(x_val_mat), tsteps, 1)
   x_val_arr <- array(data = x_val_mat, dim = x_val_dim)
 
   y_val_vec <- val_DT$value
@@ -54,7 +69,7 @@ ts_nn_preparation <- function(
 
   test_DT <- data[n_test]
   x_test_mat <- as.matrix(test_DT[, .SD, .SDcols = patterns("^value_lag")])
-  x_test_dim <- c(nrow(x_test_mat), length(lag_setting), 1)
+  x_test_dim <- c(nrow(x_test_mat), tsteps, 1)
   x_test_arr <- array(data = x_test_mat, dim = x_test_dim)
 
   y_test_vec <- test_DT$value
