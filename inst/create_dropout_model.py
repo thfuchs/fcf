16d779b9ac23c75bcc24d2196ea325ed8c4e34bc
thfuchs <thomasfuchs-re@web.de>
2020-11-21 16:41:08 +0000

    always set recurrent dropout to 0

--- inst/create_dropout_model.py
+++ inst/create_dropout_model.py
@@ -1,7 +1,7 @@
 from keras.models import Model, Sequential
 from keras import backend as K
 
-def create_dropout_model(model, dropout, recurrent_dropout):
+def create_dropout_model(model, dropout):
     """
     Create a keras function to predict with dropout (Credits to sfblake)
     model : keras model
@@ -18,10 +18,10 @@ def create_dropout_model(model, dropout, recurrent_dropout):
         # Dropout layers
         if layer["class_name"]=="Dropout":
             layer["config"]["rate"] = dropout
-        # Recurrent layers with dropout
+        # Recurrent layers with dropout (and without recurrent dropout)
         elif "dropout" in layer["config"].keys():
             layer["config"]["dropout"] = dropout
-            layer["config"]["recurrent_dropout"] = recurrent_dropout
+            layer["config"]["recurrent_dropout"] = 0
             
     # Create a new model with specified dropout
     if type(model)==Sequential:
@@ -30,6 +30,7 @@ def create_dropout_model(model, dropout, recurrent_dropout):
     else:
         # Functional
         model_dropout = Model.from_config(conf)
+    
     model_dropout.set_weights(model.get_weights())
     
     return model_dropout
